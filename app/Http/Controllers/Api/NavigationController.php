36d608dcb3d5851f13285a3a2a29fee41df23275
devangivadariya <devangivadariya1996@gmail.com>
2022-02-09 16:17:03 +0530

    Menu api

--- app/Http/Controllers/Api/NavigationController.php
+++ app/Http/Controllers/Api/NavigationController.php
@@ -26,13 +26,32 @@ class NavigationController extends Controller
 	{	 
 		if (Menu::where('id', $id)->exists())
 		{
-			 
-			$menu_list = MenuItem::join('admin_menus as m2', 'm2.id', '=', 'admin_menu_items.menu')
-			->where('admin_menu_items.menu', $id)->where('admin_menu_items.parent',0)->get(['admin_menu_items.*', 'm2.name']); 
-			  // dd($menu_list);
-			$data=array();
 			$image_path= url('/storage/uploads');
 			$link= url('');
+			
+			$menu_list = MenuItem::join('admin_menus as m2', 'm2.id', '=', 'admin_menu_items.menu')
+			->where('admin_menu_items.menu', $id)->where('admin_menu_items.parent',0)->get(['admin_menu_items.*', 'm2.name']); 
+			
+			$sub_list = MenuItem::join('admin_menus as m2', 'm2.id', '=', 'admin_menu_items.menu')->where('admin_menu_items.menu', $id)->where('admin_menu_items.parent','>=',1)->get(['admin_menu_items.*', 'm2.name']); 
+			
+			foreach($sub_list as $key1 => $result)
+			{
+				$insert_stock['id']=$result['id'];
+				$insert_stock['sub_menu_title']=$result['name'];
+				$insert_stock['multipleid']=$result['multipleid'];
+				$insert_stock['type_category']=$result['type_category'];
+				$insert_stock['label']=$result['label'];
+				$insert_stock['image']=$image_path .'/'. $result['image'];
+				$insert_stock['link']=$image_path .'/'. $result['link'];
+				$insert_stock['parent']=$result['parent'];
+				$insert_stock['sort']=$result['sort'];
+				$insert_stock['class']=$result['class'];
+				$insert_stock['menu']=$result['menu'];
+				$insert_stock['depth']=$result['depth'];
+				$data_result[$key1] = $insert_stock;
+			}
+				 
+			$data=array();			 
 			foreach($menu_list as $key => $value)
 			{
 				$data['id']=$value['id'];
@@ -47,10 +66,14 @@ class NavigationController extends Controller
 				$data['class']=$value['class'];
 				$data['menu']=$value['menu'];
 				$data['depth']=$value['depth'];
+				if($value['id'] == $data_result[$key1]['parent']){
+				$data['children']=  $data_result;
+				}
+				 
 				$blog_result[$key] = $data;   
 				
 			}
-			return response($blog_result, 200);
+			return response($blog_result , 200);
 		}
 		else
 		{
