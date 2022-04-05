9a4949cea2c988f6fb36a1e1db767a7429a56beb
vishalwebtech123 <85621500+vishalwebtech123@users.noreply.github.com>
2022-04-05 16:11:35 +0530

    add ta

--- resources/views/livewire/admin/settings.blade.php
+++ resources/views/livewire/admin/settings.blade.php
@@ -41,6 +41,24 @@
 
                                 </a>
 
+                                <a href="{{ route('taxes') }}" class="settings-option row field_style1 mb-2">
+
+                                    <div class="setting-icon">
+
+                                        <svg viewBox="0 0 20 20" class="Polaris-Icon__Svg_375hu" focusable="false" aria-hidden="true"><path fill-rule="evenodd" d="M4.5 0h11c.828 0 1.5.677 1.5 1.512v18.21a.2.2 0 0 1-.334.149l-1.664-1.515a.497.497 0 0 0-.67 0l-1.664 1.514a.497.497 0 0 1-.67 0l-1.663-1.514a.497.497 0 0 0-.67 0L8.002 19.87a.497.497 0 0 1-.67 0l-1.664-1.514a.497.497 0 0 0-.67 0l-1.664 1.559a.2.2 0 0 1-.334-.15V1.512C3 .677 3.672 0 4.5 0zm3 9a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3zm6.5 3.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm-.293-4.793a1 1 0 0 0-1.414-1.414l-6 6a1 1 0 1 0 1.414 1.414l6-6z"></path></svg>
+
+                                    </div>
+
+                                    <div class="setting-details">
+
+                                       <label>Taxes</label>
+
+                                        <p>Manage how your store charges taxes</p>
+
+                                    </div>
+
+                                </a>
+
                                <!--  <a href="{{ route('notifications') }}" class="settings-option row field_style1 mb-2">
 
                                     <div class="setting-icon">
