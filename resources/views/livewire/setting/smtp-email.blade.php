ad506d9181ff68fe50b844b39f238719ab555caa
muskan.verma <muskan@webtech-evolution.com>
2022-05-02 19:11:52 +0530

    changes

--- resources/views/livewire/setting/smtp-email.blade.php
+++ resources/views/livewire/setting/smtp-email.blade.php
@@ -1,46 +1,53 @@
 <div>
 <x-admin-layout>
-	<div class="columns four pt-3 pr-3">
-	   <h4 class="fs-15 fw-5 mb-16">MAIL Details</h4>
-	</div>  
+	<style>
+        .add-customer-part .card label {
+    
+    font-weight: 500;
+}
+.mb-10{
+    margin-bottom: 10px !important;
+}
+    </style>
     <section class="full-width flex-wrap admin-body-width setting-general-sec" wire:ignore>
         <article class="full-width">
             <div class="columns ten">
                 <article class="full-width add-customer-part bd_none">
               
                     <div class="columns eight">
+                        <h2 class="fs-20 fw-5 mb-16">Mail Details</h2>
                         <div class="card">
                             <div class="row">
                                 <label>MAIL_MAILER</label>
-                                <input type="text" wire:model="getsmtp.mailmailer" value="">
+                                <input type="text" class="mb-10" wire:model="getsmtp.mailmailer" value="">
                             </div>
                             <div class="row">
                                 <label>MAIL_HOST</label>
-                                <input type="email" wire:model="getsmtp.mailhost" value="">
+                                <input type="email" class="mb-10" wire:model="getsmtp.mailhost" value="">
                             </div>
                             <div class="row">
                                 <label>MAIL_PORT</label>
-                                <input type="email" wire:model="getsmtp.mailport" value="">
+                                <input type="email" class="mb-10" wire:model="getsmtp.mailport" value="">
                             </div>
                             <div class="row">
                                 <label>MAIL_USERNAME</label>
-                                <input type="email" wire:model="getsmtp.mailusername" value="">
+                                <input type="email" class="mb-10" wire:model="getsmtp.mailusername" value="">
                             </div>
                             <div class="row">
                                 <label>MAIL_PASSWORD</label>
-                                <input type="email" wire:model="getsmtp.mailpassword" value="">
+                                <input type="email" class="mb-10" wire:model="getsmtp.mailpassword" value="">
                             </div>
-                            <div class="row">
+                            {{-- <div class="row">
                                 <label>MAIL_ENCRYPTION</label>
                                 <input type="email" wire:model="getsmtp.mailformaddress" value="">
-                            </div>
+                            </div> --}}
                             <div class="row">
                                 <label>MAIL_FROM_ADDRESS</label>
-                                <input type="email" wire:model="getsmtp.mailfrom_name" value="">
+                                <input type="email" class="mb-10" wire:model="getsmtp.mailfrom_name" value="">
                             </div>
                             <div class="row">
                                 <label>MAIL_FROM_NAME</label>
-                                <input type="email" wire:model="getsmtp.mailfrom_encypation" value="">
+                                <input type="email" class="mb-10" wire:model="getsmtp.mailfrom_encypation" value="">
                             </div>
                         </div>
                     </div>
@@ -61,8 +68,8 @@
 	    @endif
 
 	</div>
-    <section class="full-width flex-wrap admin-body-width" wire:ignore>
-        <div class="page-bottom-btn flex-end">
+    <section class=" admin-body-width" wire:ignore>
+        <div class="page-bottom-btn flex-end columns eight">
             <button wire:click="updatestore" class="button">Save</button>
         </div>
     </section>
