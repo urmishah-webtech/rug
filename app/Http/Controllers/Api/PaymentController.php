1268ff90a1b53ad025fcced3bb07c7b0000588a5
Priya <priya@webtech-evolution.com>
2022-05-05 15:50:12 +0530

    Update PaymentController.php

--- app/Http/Controllers/Api/PaymentController.php
+++ app/Http/Controllers/Api/PaymentController.php
@@ -241,23 +241,32 @@ class PaymentController extends Controller
         }   
     }
 
-    public function get_thankyou($userid){
+     public function get_thankyou($userid){
 
         $ordercheck = Orders::where('user_id',$userid)->count();
         $user_detail = User::where('id', $userid)->first();
         if($ordercheck != 0){
             $order = Orders::where('user_id',$userid)->orderBy('id', 'DESC')->first();
             
-            $order_item = order_item::with('media_product')->with('variant_product')->with('order_product')->where('order_id',$order->id)->orderBy('id', 'DESC')->get();
+            $order_item = order_item::with('media_product')->with(['variant_product' => function($q) {
+                return $q->with('variantmediafirst');
+            }])->with('order_product')->where('order_id',$order->id)->orderBy('id', 'DESC')->get();
 
             $image_path= env('IMAGE_PATH');
 
             $finalamount = 0;
                 $i=0;
+                
+               
             foreach ($order_item as $key => $result)
             {
+                if(!$result->variant_product->isEmpty() && isset($result->variant_product[0]) && !empty($result->variant_product[0]->variantmediafirst) && !empty($result->variant_product[0]->variantmediafirst->image)) {
+                    $order_item[$key]['image'] = $image_path.$result->variant_product[0]->variantmediafirst->image;
+                } else {
+                    $order_item[$key]['image'] = $image_path.$result['media_product'][0]['image'];
+                }
+               
                 $order_item[$key]['title'] = $result['order_product'][0]['title'];
-                $order_item[$key]['image'] = $image_path.$result['media_product'][0]['image'];
                 $Totalamount = ($result->stock * $result->price);
                 $finalamount += $Totalamount;
                 
