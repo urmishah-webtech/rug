4f63c7ae52c9ed8a556dee474495b9f53eecf7a4
Priya <priya@webtech-evolution.com>
2022-05-10 14:37:02 +0530

    delete-shipping api

--- app/Http/Controllers/Api/CheckoutController.php
+++ app/Http/Controllers/Api/CheckoutController.php
@@ -46,4 +46,13 @@ class CheckoutController extends Controller
             'dataget' => $data,
             ]);
     }
+    public function deleteShipping($id)
+    {
+    	$deleted = CustomerAddress::where('id', $id)->delete();
+    	return response()->json([
+		    'success' => true, 
+		    'message'=> 'deleted sucessfully.',
+		    'data'    => $deleted,
+            ]);
+    }
 }
