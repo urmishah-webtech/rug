201596f63624a381570cfa708b24d1ce8c3cba6a
urmishah-webtech <urmi@webtech-evolution.com>
2022-05-23 15:12:37 +0530

    api changes

--- app/Http/Controllers/Api/GuestCheckout.php
+++ app/Http/Controllers/Api/GuestCheckout.php
@@ -48,24 +48,34 @@ use Illuminate\Auth\Passwords\DatabaseTokenRepository;
 
 use Illuminate\Support\Facades\Session;
 
+use Illuminate\Support\Facades\Validator; // For Validation
+
+use App\Models\Cart;
+
 class GuestCheckout extends Component
 {
     public function GuestUserSave(Request $request)
     {
+      
         if($request->account_type==1){
-            $request->validate([
+           
+            $validation = Validator::make($request->all() ,[
+
                 'g_email' => 'required|email',
                 'g_first_name' => 'required',
                 'g_last_name' => 'required',
                 'g_mobile_number' => 'required',
                 'password' =>  ['required', 'min:8', 'regex:/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[^\w\s]).{8,}$/'],
             ]);
-
+            if($validation->fails()) {
+                return response()->json(['message' => $validation->errors(), 'success' => false]);
+            }
             $user = User::where('email', $request->g_email)->first();
+            
             if($user) {
                 return response()->json(['message' => 'Email already exist!', 'success' => false]);
             }
-    
+           
             $user = User::create([
                 'first_name' => $request->g_first_name,
                 'last_name' => $request->g_last_name,
@@ -73,8 +83,12 @@ class GuestCheckout extends Component
                 'mobile_number' => $request->g_mobile_number,
                 'password' => Hash::make($request->password),
                 'account_type' => '2',
-                // 'session_id' => $request->session_id
+                // 'session_id' =>
+                
             ]);
+        
+            $user['token'] = $user->createToken('appToken')->accessToken;
+            $payment = Cart::where('session_id', $request->session_id)->update(['user_id' => $user->id,'session_id' => '']);
 
             return response()->json(['message' => 'Record Created!!', 'success' => true, 'user'=>$user ]);
 
